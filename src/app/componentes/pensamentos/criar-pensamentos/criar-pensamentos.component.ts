e9e8151e453967a30ae5459647af69d892f8666f
erick-augusto <erick_aug@hotmail.com>
2025-08-18 19:16:29 -0300

    Ligando os dados da página com um formulário

--- src/app/componentes/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamentos',
@@ -10,19 +11,27 @@ import { Router } from '@angular/router';
 })
 export class CriarPensamentosComponent implements OnInit {
 
-    pensamento: Pensamento = {
-      conteudo: '',
-      autoria: '',
-      modelo: 'modelo1'
-    }
+    formulario!:FormGroup;
 
-  constructor(private service: PensamentoService, private router: Router) { }
+  constructor(private service: PensamentoService, private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      conteudo: ['Formulario reativo'],
+      autoria: [''],
+      modelo: ['modelo1']
+    });
+
+    //Forma opcional de criar o formulário
+    /*this.formulario = new FormGroup({
+      conteudo: new FormControl(''),
+      autoria: new FormControl(''),
+      modelo: new FormControl('')
+    })*/
   }
 
   criarPensamento(){
-    this.service.criar(this.pensamento).subscribe((pensamento: Pensamento) => {
+    this.service.criar(this.formulario.value).subscribe((pensamento: Pensamento) => {
       this.router.navigate(['/listarPensamento']);
     });
   }
